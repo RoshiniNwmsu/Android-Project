f670d49572e679da2afac16bf040cf7ffb53a247
Ashwanth_s559214 <123043659+AshwanthS559214@users.noreply.github.com>
2023-10-29 00:26:56 -0500

    Update RegisterPage.java

--- News Pulse/NewsPulse/app/src/main/java/com/example/newspulse/RegisterPage.java
+++ News Pulse/NewsPulse/app/src/main/java/com/example/newspulse/RegisterPage.java
@@ -1,31 +1,150 @@
 package com.example.newspulse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class RegisterPage extends AppCompatActivity {
-    private TextView loginButton;
+    public static final String USERS = "User";
+    public static final String ARTICLES = "Articles";
+
+    EditText et_email, et_password, et_confirmPassword, et_username;
+    Button btn_Register;
+    TextView tv_loginBtn;
+
+    String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+            "[a-zA-Z0-9_+&-]+)@" +
+            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+            "A-Z]{2,7}$";
+
+    Pattern pat = Pattern.compile(emailRegex);
+
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
 
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        et_confirmPassword = findViewById(R.id.et_confirmPassword);
+        btn_Register = findViewById(R.id.btn_register);
+        tv_loginBtn = findViewById(R.id.tv_loginButton);
 
-        // Find the Login TextView by its ID
-        loginButton = findViewById(R.id.tv_loginButton);
+        progressDialog = new ProgressDialog(this);
 
-        // Set an OnClickListener for the Login TextView
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        tv_loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create an Intent to navigate to the LoginPage (replace LoginPage.class with your actual login activity)
-                Intent intent = new Intent(RegisterPage.this, LoginPage.class);
-                startActivity(intent); // Start the LoginPage
+                startActivity(new Intent(RegisterPage.this, LoginPage.class));
             }
+        });
 
-    });
-}}
+        btn_Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerformAuth();
+            }
+        });
+
+    }
+
+    private void PerformAuth() {
+        String email = et_email.getText().toString();
+        String password = et_password.getText().toString();
+        String confirmPassword = et_confirmPassword.getText().toString();
+        String username = et_username.getText().toString();
+
+        if (email.isEmpty()) {
+            et_email.setError("Please Enter Email");
+            return;
+        } else if (!pat.matcher(email).matches()) {
+            et_email.setError("Please Enter a valid Email");
+            return;
+        } else if (password.isEmpty()) {
+            et_password.setError("Please input Password");
+            return;
+        } else if (password.length() < 6) {
+            et_password.setError("Password too short");
+            return;
+        } else if (!confirmPassword.equals(password)) {
+            et_confirmPassword.setError("Password doesn't matches");
+            return;
+        } else {
+            progressDialog.setMessage("Creating your Account....");
+            progressDialog.setTitle("Creating");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+
+                        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                        String userId = firebaseUser.getUid();
+
+                        reference = FirebaseDatabase.getInstance().getReference().child(RegisterPage.USERS).child(userId);
+                        HashMap<String, String> hashMap = new HashMap<>();
+                        hashMap.put("id", userId);
+                        hashMap.put("username", username);
+                        hashMap.put("imageUrl", "default");
+                        hashMap.put("search", username.toLowerCase());
+
+                        reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    sendUserToMainActivity();
+                                }
+                            }
+                        });
+
+
+                        Toast.makeText(RegisterPage.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterPage.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToMainActivity() {
+        Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
+}
